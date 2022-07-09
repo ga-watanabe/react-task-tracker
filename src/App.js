12cb6ba9201e3c0b65fedc7319b86165e781c8da
Gakuto Watanabe <g_watanabe@ga-tech.co.jp>
2022-07-09 18:42:07 +0900

    post to db

--- src/App.js
+++ src/App.js
@@ -23,11 +23,18 @@ function App() {
     return data
   }
 
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() *10000) + 1
-    const newTask = { id, ...task }
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
 
-    setTasks([...tasks, newTask])
+    const data = await res.json()
+
+    setTasks([...tasks, data])
   }
 
   const deleteTask = async (id) => {
