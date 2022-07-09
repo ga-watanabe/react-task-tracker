370b5c5001d97d677c9b2f4014bcadba9f3f1ccc
Gakuto Watanabe <g_watanabe@ga-tech.co.jp>
2022-07-09 20:22:07 +0900

    useLocation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,30 @@
-import PropTypes from 'prop-types'
-import Button from './Button'
+import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
+import Button from "./Button";
 
 const Header = ({ title, onAdd, showAddTask }) => {
+  const location = useLocation();
+
   return (
-    <header className='header'>
+    <header className="header">
       <h1>{title}</h1>
-      <Button
-        text={showAddTask ? 'Close' : 'Add'}
-        color={showAddTask ? 'red' : 'green'}
-        onClick={onAdd} />
+      {location.pathname === "/" && (
+        <Button
+          text={showAddTask ? "Close" : "Add"}
+          color={showAddTask ? "red" : "green"}
+          onClick={onAdd}
+        />
+      )}
     </header>
-  )
-}
+  );
+};
 
 Header.defaultProps = {
-  title: 'Default Title'
-}
+  title: "Default Title",
+};
 
 Header.propTypes = {
-  title: PropTypes.string.isRequired
-}
+  title: PropTypes.string.isRequired,
+};
 
-export default Header
+export default Header;
