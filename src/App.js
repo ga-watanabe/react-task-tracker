4b8753f52796b5c26adf7568919930adde3b242d
Gakuto Watanabe <g_watanabe@ga-tech.co.jp>
2022-07-09 15:17:53 +0900

    add delete button

--- src/App.js
+++ src/App.js
@@ -26,10 +26,16 @@ function App() {
     ]
   )
 
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="container">
       <Header title="Task Tracker" />
-      <Tasks tasks={tasks}/>
+      <Tasks
+        tasks={tasks}
+        onDelete={deleteTask} />
     </div>
   );
 }
