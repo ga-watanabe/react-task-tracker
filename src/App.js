3302313412efb997e4088791c32a1dd0b7b29ea6
Gakuto Watanabe <g_watanabe@ga-tech.co.jp>
2022-07-09 16:02:01 +0900

    add form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
   const [tasks, setTasks] = useState(
@@ -26,6 +27,13 @@ function App() {
     ]
   )
 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() *10000) + 1
+    const newTask = { id, ...task }
+
+    setTasks([...tasks, newTask])
+  }
+
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
@@ -39,6 +47,7 @@ function App() {
   return (
     <div className="container">
       <Header title="Task Tracker" />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ?
         <Tasks
           tasks={tasks}
